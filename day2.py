06730ec349beb55ce173129e28df1f63ab9c85b4
Mohamed, Amr (DI SW PLM STS SPDEV SYS XTS OPS) <mohamedamr@siemens.com>
2025-12-02 16:23:19 +0000

    final changes to part 2

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-def part1():
+'''def part1():
     with open("day2.txt", "r") as f:
         f = f.readline()
         ranges = f.split(",")
@@ -15,8 +15,50 @@ def part1():
 
 
         print(sum(invalid_IDs))
-                    
+                    '''
                 
+def part2():
+    with open("day2.txt", "r") as f:
+        f = f.readline()
+        ranges = f.split(",")
+        invalid_IDs = []
+        for rng in ranges:
+            start, end = int(rng.split("-")[0]), int(rng.split("-")[1])
+            for i in range(start, end + 1):
+                if len(str(i)) %2  == 0:
+                    first_half = str(i)[:len(str(i))//2]
+                    second_half = str(i)[len(str(i))//2:]
+                    if first_half == second_half:
+                        invalid_IDs.append(i)
+                        print(i)
+                        continue
+                k = 0
+                i = str(i)
+                for j in range(len(i)-1):
+                    if i[k] == i[j+1]: k+=1
+                    else: k=0
+                if k != 0:
+                    if k> len(i)/2:
+                        invalid_IDs.append(int(i))
+                        print(i)
+    print(sum(invalid_IDs))                
+
+
+part2()
+
+     
+"""
+11-22
+create a list that starts with 11 and ends with 22
+find something that checks number of consequtive identical characters
+or filter it all out take evertrihng out you'd get this
+111213141516171819202122
+at i
+after finding a number append it to the list
+for i in range(11,23):
+    print(i)
+
+
 def part2():
     with open("day2.txt", "r") as f:
         f = f.readline()
@@ -36,27 +78,22 @@ def part2():
                 if state == True:
                     break
                 k = 0
-                for j in range(i):
-                    k+=1 if i[k] == i[j+1] else -k
+                i = str(i)
+                m=0
+                l=[""]
+                for j in range(len(i)-1):
+                    l[m] += i[j]
+                    if i[k] == i[j+1]:
+                        
+
+                    k+=1  else -k
                 if k != 0:
-                    invalid_IDs.append(i)
+                    if k
+                    invalid_IDs.append(int(i))
                     print(i)
     print(sum(invalid_IDs))                
 
 
-
-
-     
-"""
-11-22
-create a list that starts with 11 and ends with 22
-find something that checks number of consequtive identical characters
-or filter it all out take evertrihng out you'd get this
-111213141516171819202122
-at i
-after finding a number append it to the list
-for i in range(11,23):
-    print(i)
+    
 """
 
-part1()
