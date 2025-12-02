f5389a419d9af1f15e121c7c26aebde054b749ec
Amr15066 <amr22126@outlook.com>
2025-12-02 21:42:16 +0000

    trying to fix my attempt

--- day2.py
+++ day2.py
@@ -33,20 +33,52 @@ def part2():
                         print(i)
                         continue
                 k = 0
+                m = 0
+                if i == 1221221:
+                    print("here")
                 i = str(i)
                 for j in range(len(i)-1):
-                    if i[k] == i[j+1]: k+=1
-                    else: k=0
-                if k != 0:
-                    if k> len(i)/2:
-                        invalid_IDs.append(int(i))
-                        print(i)
+                    if i[k] == i[j+1]: 
+                        k+=1
+                    else:
+                        m+=1
+                        k=0
+
+                if k> int(len(i)/2) :
+                    if (k % 2 != 0 and len(i) % 2 != 0 ):
+                        continue
+                    invalid_IDs.append(int(i))
+                    print(i)
     print(sum(invalid_IDs))                
 
 
+
+def other_method():
+    with open("day2.txt", "r") as f:
+        f = f.readline()
+        ranges = f.split(",")
+        invalid_IDs = []
+        for rng in ranges:
+            start, end = int(rng.split("-")[0]), int(rng.split("-")[1])
+            for i in range(start, end + 1):
+                i = str(i)
+                for j in range(len(i)-1):
+                    if len(i) % (j+2) == 0:
+                        chunks = divide_into_chunks(i,j+2)
+                        if all(chunk == chunks[0] for chunk in chunks):
+                            invalid_IDs.append(int(i))
+                            print(i)
+                            break
+    print(sum(invalid_IDs))   
+
+def divide_into_chunks(number, divisions):
+    return list(number[i:i + len(number) // divisions] for i in range(0, len(number), len(number) // divisions))
+
+        
+#other_method()
 part2()
 
-     
+
 """
 11-22
 create a list that starts with 11 and ends with 22
